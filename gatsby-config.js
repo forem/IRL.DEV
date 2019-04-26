c83a14c493263218e5bd9f66dcb160ec164fe225
Ali Spittel <ali07cat07@gmail.com>
2019-04-26 15:28:12 -0400

    update previewcopy

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 module.exports = {
   siteMetadata: {
     title: `DEV Real Life Events`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `There's nothing more awesome than meeting your internet friends IRL.`,
     author: `@gatsbyjs`,
   },
   plugins: [
